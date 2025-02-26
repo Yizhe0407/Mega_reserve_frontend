7e3ead5ebe4814061bca1f2ae987cb417de1db3a
yizhe <liaoyizhe75@gmail.com>
2025-02-26 14:32:51 +0800

    Change weekly height between calendar component

--- src/components/ui/calendar.jsx
+++ src/components/ui/calendar.jsx
@@ -19,8 +19,8 @@ function Calendar({
       className={cn("p-3", className)}
       locale={zhTW}
       classNames={{
-        months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
-        month: "space-y-4",
+        months: "flex flex-col sm:flex-row space-y-1 sm:space-x-2 sm:space-y-0",
+        month: "space-y-1",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
         nav: "space-x-1 flex items-center",
@@ -30,11 +30,11 @@ function Calendar({
         ),
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
-        table: "w-full border-collapse space-y-1",
+        table: "w-full border-collapse space-y-0.5",
         head_row: "flex",
         head_cell:
           "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]",
-        row: "flex w-full mt-2",
+        row: "flex w-full mt-0.5",
         cell: cn(
           "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-end)]:rounded-r-md",
           props.mode === "range"
